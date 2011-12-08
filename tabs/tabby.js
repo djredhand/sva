acd3746136697da26b59903618ce15d87ad58cdd
james <jed.davis13@gmail.com>
2011-12-08 17:32:27 -0500

    more stuff added

--- tabs/tabby.js
+++ tabs/tabby.js
@@ -2,10 +2,6 @@
 (function ($){
 	$.fn.tabby = function(arguments){
 		var defaults = {
-			width		: 300,
-			height		: 200,
-			background	: '#999',
-			border		: '1px solid #000',
 			defaultTab	: ''//must include hash '#'
 		};
 		var options		= $.extend(defaults, arguments);
@@ -13,9 +9,15 @@
 		var divIds		= [];
 		var obj			= $(this); 
 		var _display	= options.defaultTab//arguments from user;
-		$("a",obj).each(function(){
-			var href = this.href.split('#')[1]
-			$(this).parent().click(function(){showDiv(href)} );
+		
+		$("a",obj).each(function(){			
+			var url = this.href.split('#')[1];
+			$(this).parent().click(function(){
+				showDiv(url);
+				selected($(this).parent(), $(this));
+				window.location.hash =  url;
+			} );
+			
         if(this.href.match(/#/)) { 
           links.push(this); 
           divIds.push("#"+this.href.split('#')[1]);//push with hash to append url
@@ -24,45 +26,41 @@
 	  
 	  //find default tab and display - if no default display the 1st div
 	  for(items in divIds){
+		  //console.log(divIds[items])
 		  $(divIds[items]).css({display: 'none'});
 		  if(options.defaultTab == divIds[items]){
 			 _display = "match";
-		  }
+		  };
 		  //log the div elems for the tabs - console.log($(divIds[items]) );
-	  }
-	  // If there is no match with the default param, then show 1st div
+	  };
+	  //If there is no match with the default param, then show 1st div
 	  _display == "match" ? $(options.defaultTab).css({display: 'block'}) : $(divIds[0]).css({display: 'block'});
+	 // console.log(_display)
 	  
-	  
+	   if(location.href.split('#')[1] != ""){
+		  var link = location.href.split('#')[1];
+		  showDiv(link);
+	  }
+
+	 
 	  //Click handler for the list items
 	  function showDiv(link){
-		  console.log("divIds is: " + divIds)
 		  for (items in divIds){
 			  $(divIds[items]).css({display: 'none'});
 			  if (link == divIds[items].split('#')[1]){
 				  _display = "showDivMatch";
-				  $(divIds[items]).addClass('selected');
-			  }else{
-				  $(divIds[items]).removeClass('selected');
+				  selected($(links[items]).parent().parent(), $(links[items]).parent());
 			  }
 		  }
 		  //need to crate a showDefault() function 
 		  _display == "showDivMatch" ? $('#'+link).css({display: 'block'}) : $(divIds[0]).css({display: 'block'});
-		  console.log(_display);
 	  }
 	  
-	  if(location.href.split('#')[1] != ""){
-		  var link = location.href.split('#')[1];
-		  showDiv(link);
-	  }
-	  
-	  console.log(links);//available links in the list 
-	  console.log(options.defaultTab);//the default tab
-	  console.log(divIds);//the divs Ids
-	  console.log(divIds[2].split('#')[1]); //the div id without hash to query in the doc
-	  console.log('_display is: ' + _display);
-	  console.log('location.href is: ' + location.href.split('#')[1]);
-	  
-	  
+	  function selected(ul, li){
+		  ul.children().each(function(){
+			  $(this).removeAttr('class');
+		  });
+		  li.addClass('selected');
+	  }	  
 	};
 })(jQuery);
